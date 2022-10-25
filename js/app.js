e002ad466918ed5dce6acfce5a98182d47acab7f
FatimahAlIbrahim <fatimah.al.ibrahim.1997@gmail.com>
2022-10-25 10:47:23 +0300

    feat: dynamically generate nav, update active when scrolling, and scroll when clicking on menu item

--- js/app.js
+++ js/app.js
@@ -59,3 +59,51 @@
 
 // Set sections as active
 
+/////////////////////////////////////////
+
+// Dynamicly build the nav bar
+const sections = document.getElementsByTagName("section");
+const documentFragment = document.createDocumentFragment();
+
+for (const section of sections) {
+    let listItemElement = document.createElement('li');
+    let linkElement = document.createElement('a');
+
+    linkElement.textContent = section.dataset.nav;
+    linkElement.classList.add("menu__link");
+    if(section.id[7] == "1"){
+        linkElement.classList.add("menu__link--active");
+    }
+    linkElement.href = "#"+section.id;
+
+    listItemElement.appendChild(linkElement)
+    documentFragment.appendChild(listItemElement);
+}
+//documentFragment.children[0].children[0].classList.add("menu__link--active");
+document.getElementById("navbar__list").appendChild(documentFragment);
+
+// add scroll event
+document.addEventListener("scroll", (event) => {
+    for (const section of sections) {
+        if(section.getBoundingClientRect().y <= 480 && section.getBoundingClientRect().y > -130){
+            section.classList.add("active-section");
+            document.querySelector(".menu__link--active").classList.remove("menu__link--active");
+            let listItems = document.getElementById("navbar__list").children;
+            let currentSection = Number(section.id[7]);
+            listItems[(currentSection - 1)].firstElementChild.classList.add("menu__link--active");
+        }else{
+            section.classList.remove("active-section");
+        }
+    }
+});
+
+// add on click event
+document.getElementById("navbar__list").addEventListener("click", (event) => {
+    if(event.target.nodeName.toLowerCase() === "a"){
+        event.preventDefault();
+        let query = `section[data-nav = '${event.target.textContent}']`;
+        let desiredSection = document.querySelector(query);
+        desiredSection.scrollIntoView({behavior: "smooth"});
+    }
+}) 
+
